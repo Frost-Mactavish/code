46653c5a2830f06d5bea600a35326eaa0b6e8b49
Frost <marlysragonesevyy52@gmail.com>
2024-11-22 16:17:30 +0800

    更新了训练测试脚本，能够输出标准错误，并且输出会同时显示在终端和重定向的文件中

--- lora/train.sh
+++ lora/train.sh
@@ -6,7 +6,7 @@ conda activate mytorch
 
 filename="train_$(date '+%m%d-%H%M').txt"
 
-python -u train.py --phase 'base' > ./log/$filename
+python -u train.py --phase 'base' 2>&1 | tee log/$filename
 
-python -u train.py --phase 'inc' > ./log/$filename
+python -u train.py --phase 'inc' 2>&1 | tee -a log/$filename
 
